d456d762084066e2920e5496ed39101f1d9f06f3
shrria <58872002+shrria@users.noreply.github.com>
2022-01-26 23:37:43 +0700

    initial commit (frontend)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      : Hello My Tutor!
+      <div className="container">: Hello My Student!</div>
+    </div>
+  );
+}
+
+export default App;
