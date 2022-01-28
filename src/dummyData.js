5572b493957db6c31a99ab2e47775e82b819b741
shrria <58872002+shrria@users.noreply.github.com>
2022-01-28 21:12:03 +0700

    Create dummyData.js
    dummy Data for sidebar

---
+++ src/dummyData.js
@@ -0,0 +1,49 @@
+export const apptData = [
+  {
+    fullname: "Yotsuba Nakano",
+    username: "@yotsuba428",
+    day: "Monday",
+    date: "17 Jan 2022",
+    time: "16:30-18:30",
+    location: "at Living Room",
+    imgURL:
+      "https://somoskudasai.com/wp-content/uploads/2020/12/yotsuba-nakano-somoskudasai.jpg",
+  },
+  {
+    fullname: "Miku Nakano",
+    username: "@n.miku0505",
+    day: "Tuesday",
+    date: "18 Jan 2022",
+    time: "10:00-12:00",
+    location: "at School Library",
+    imgURL:
+      "https://i.pinimg.com/736x/1c/4f/4c/1c4f4c7010a0b7ca14e32baa83fe2181.jpg",
+  },
+];
+
+export const classData = [
+  {
+    subject: "Maths",
+    location: "@SiamDiscovery",
+    time: "10:00-12:00",
+  },
+  {
+    subject: "English",
+    location: "@SiamKitt(HollyCoffee)",
+    time: "14:00-16:00",
+  },
+  {
+    subject: "Physics",
+    location: "@SamyanMitrtown",
+    time: "16:30-18:00",
+  },
+];
+
+export const msgData = [
+  {
+    fullname: "Itsuki Nakano",
+    username: "@it-tsuki5",
+    content: "Uesugu-kun, we'll be waiting at Cafe.",
+    imgURL: "http://pm1.narvii.com/7164/138f64a909260c53492ff549125858e140372130r1-750-750v2_00.jpg"
+  },
+];
