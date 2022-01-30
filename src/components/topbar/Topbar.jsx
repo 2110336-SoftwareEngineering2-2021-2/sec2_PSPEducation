1b313723d01004e58ba12364e0b6a63a8d43e25e
shrria <58872002+shrria@users.noreply.github.com>
2022-01-31 00:21:50 +0700

    Add searchbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,6 +4,7 @@ import {
   AppsOutlined,
   EmailOutlined,
   NotificationsNoneOutlined,
+  SearchOutlined,
 } from "@mui/icons-material";
 import { userData } from "../../dummyData";
 
@@ -18,24 +19,35 @@ export default function Topbar() {
             <span className="topLeftauthor">Powered by PSPEducaion</span>
           </div>
         </div>
+        <div className="topCenter">
+          <div className="searchbar">
+            <SearchOutlined className="searchIcon" />
+            <input
+              placeholder="Search for course, tutor or subject"
+              className="searchInput"
+            />
+          </div>
+        </div>
         <div className="topRight">
-          <div className="topbarIconContainer">
-            <AppsOutlined />
-          </div>
-          <div className="topbarIconContainer">
-            <EmailOutlined />
-            <span className="topIconBadge">3</span>
-          </div>
-          <div className="topbarIconContainer">
-            <NotificationsNoneOutlined />
-            <span className="topIconBadge">8</span>
-          </div>
-          <img src={imgSrc} alt="" className="topAvatar" />
-          {/* <div className="userInfo">
-            <span className="fullName">{userData.fullname}</span>
+          <div className="topRightContainer">
+            <div className="topbarIconContainer">
+              <AppsOutlined />
+            </div>
+            <div className="topbarIconContainer">
+              <EmailOutlined />
+              <span className="topIconBadge">3</span>
+            </div>
+            <div className="topbarIconContainer">
+              <NotificationsNoneOutlined />
+              <span className="topIconBadge">8</span>
+            </div>
+            <img src={imgSrc} alt="" className="topAvatar" />
+            <div className="userInfo">
+            {/* <span className="fullName">{userData.fullname}</span>
             <span className="userName">{userData.username}</span>
-            <span>credit: {userData.credit_balance}</span>
-          </div> */}
+            <span>credit: {userData.credit_balance}</span> */}
+          </div>
+          </div>
         </div>
       </div>
     </div>
