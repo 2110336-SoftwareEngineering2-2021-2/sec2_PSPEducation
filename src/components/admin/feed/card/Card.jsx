c0b80c163ba138c9cd94a7567e174afad82fb444
shrria <58872002+shrria@users.noreply.github.com>
2022-01-31 03:51:07 +0700

    Create cards (assignments of admin)
    There's only a card for tutor validation for now. :<

---
+++ src/components/admin/feed/card/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./card.css";
+// import { StarRounded, StarOutlineRounded } from "@mui/icons-material";
+
+export default function Card() {
+  const imgSrc = "https://source.unsplash.com/random/300×300/?face";
+  return (
+    <div className="card">
+      <div className="cardContent">
+        <div className="cardImageContainer">
+          <img src={imgSrc} alt="" className="cardImageAvatar" />
+        </div>
+        <div className="tutorName">
+          <span className="fullName">Tutor Name1</span>
+          <span className="profession">SE II</span>
+        </div>
+
+        <div className="cardButton">
+          <button className="cardButtonView">view</button>
+          <button className="cardButtonRemove">remove</button>
+        </div>
+      </div>
+    </div>
+  );
+}
