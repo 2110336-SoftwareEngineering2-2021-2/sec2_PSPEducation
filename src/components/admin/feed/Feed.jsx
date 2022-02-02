3879b874d95a9a0628adcc8464ed35ec689f2556
shrria <58872002+shrria@users.noreply.github.com>
2022-02-03 00:16:37 +0700

    Test feed display

--- src/components/admin/feed/Feed.jsx
+++ src/components/admin/feed/Feed.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import "./feed.css";
 import TutorValidationCard from "./card/tutorValidation/TutorValidationCard";
+import UserReportCard from "./card/userReport/UserReportCard";
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
+        <UserReportCard />
         <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
+        <UserReportCard />
+        <UserReportCard />
         <TutorValidationCard />
         <TutorValidationCard />
       </div>
