2a3025d4c829d483da1a0ee2c2ed48a72fc6a15a
shrria <58872002+shrria@users.noreply.github.com>
2022-02-05 00:36:14 +0700

    Add /student to index
    Route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminHome from "./pages/admin/home/AdminHome";
 import TutorHome from "./pages/tutor/home/TutorHome";
+import StudentHome from "./pages/student/home/StudentHome";
 import App from "./App";
 
 ReactDOM.render(
@@ -10,8 +11,9 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/tutor" element={<TutorHome />} />
         <Route path="/admin" element={<AdminHome />} />
+        <Route path="/tutor" element={<TutorHome />} />
+        <Route path="/student" element={<StudentHome />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
