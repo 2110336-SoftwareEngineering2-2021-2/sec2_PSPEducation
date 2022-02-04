1b605de35f5d45736c18a164e26e0c91381a1417
shrria <58872002+shrria@users.noreply.github.com>
2022-02-04 22:41:23 +0700

    Edit feed components
    Cards

--- src/components/admin/feed/Feed.jsx
+++ src/components/admin/feed/Feed.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import "./feed.css";
-import TutorValidationCard from "./card/tutorValidation/TutorValidationCard";
-import UserReportCard from "./card/userReport/UserReportCard";
+import Cards from "./Cards";
 
 export default function Feed() {
   return (
     <div className="feed">
-      <div className="feedWrapper">
-        <UserReportCard />
-        <TutorValidationCard />
-        <UserReportCard />
-        <UserReportCard />
-        <TutorValidationCard />
-        <TutorValidationCard />
-      </div>
+      <Cards />
     </div>
   );
 }
