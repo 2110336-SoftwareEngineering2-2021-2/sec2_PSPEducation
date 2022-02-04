02e152fae1f83542f5e83135cc4f25dea3cf0b7b
shrria <58872002+shrria@users.noreply.github.com>
2022-02-05 00:43:50 +0700

    Create StudentFeed

---
+++ src/components/student/feed/StudentFeed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./studentFeed.css";
+import AdminCards from "./AdminCards";
+
+export default function AdminFeed() {
+  return (
+    <div className="feed">
+      <AdminCards />
+    </div>
+  );
+}
