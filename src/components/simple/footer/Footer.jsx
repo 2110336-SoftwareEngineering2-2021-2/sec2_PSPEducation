07ccb9de0bb68582c89f737ea1e3479305d164b8
shrria <58872002+shrria@users.noreply.github.com>
2022-02-08 03:04:54 +0700

    Create Footer, Navber
    existing for Plain pages such as Login, Register, ...

---
+++ src/components/simple/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div className="footerWrapper">
+          <span id="platform-version-text">
+            1.10.2.1{" "}
+            <a href="privacypolicy-2021.html" className="platform-policy">
+              Privacy Policy
+            </a>{" "}
+            | updated on Feb 7 <sup>th</sup>, 2022
+          </span>
+          <span>
+            <span className="platform-name">WhereIsMyTutor?</span> is operated
+            by PSP Education, Chulalongkorn University
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
