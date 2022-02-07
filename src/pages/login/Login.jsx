c6e2bf62306061222ea37dc7deb901b8b5dc5c3c
shrria <58872002+shrria@users.noreply.github.com>
2022-02-08 03:05:59 +0700

    Update Login (Navbar, Footer)

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,48 +1,20 @@
-import LoginLeftside from "../../components/login/loginLeftside";
-import LoginRightside from "../../components/login/loginRightside";
+import LoginLeftside from "../../components/login/loginCardLeft";
+import LoginRightside from "../../components/login/loginCardRight";
+import Navbar from "../../components/simple/navbar/Navbar";
+import Footer from "../../components/simple/footer/Footer";
 import "./login.css";
 
 export default function Login() {
   return (
     <>
       <div className="login">
-        <FakeNavbar />
+        <Navbar />
         <div className="loginWrapper">
-          {/* <h1 className="loginTitle">Login as a Tutor</h1> */}
           <div className="loginCard">
             <LoginLeftside />
             <LoginRightside />
           </div>
-          <div className="loginFooter">
-            <span id="platform-version-text">
-              1.10.2.1{" "}
-              <a href="privacypolicy-2021.html" target="_blank">
-                Privacy Policy
-              </a>{" "}
-              | updated on Feb 7 <sup>th</sup>, 2022
-            </span>
-            <span>
-              <span className="platform-name">WhereIsMyTutor?</span> are
-              operated by PSP Education Center, Chulalongkorn University
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function FakeNavbar() {
-  return (
-    <>
-      <div className="topbar">
-        <div className="topbarWrapper">
-          <div className="topLeft">
-            <div className="topLeftContainer">
-              <span className="topLeftlogo">WhereIsMyTutor?</span>
-              <span className="topLeftauthor">Powered by PSPEducaion</span>
-            </div>
-          </div>
+          <Footer />
         </div>
       </div>
     </>
