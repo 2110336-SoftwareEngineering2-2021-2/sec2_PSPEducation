36ae2b2b3be93eb275985c6ed786764775cd1872
shrria <58872002+shrria@users.noreply.github.com>
2022-02-12 05:04:04 +0700

    Divide conditional display for card

--- src/components/admin/feed/AdminCards.jsx
+++ src/components/admin/feed/AdminCards.jsx
@@ -4,54 +4,13 @@ import TutorValidationCard from "./admin-card-components/TutorValidationCard";
 import UserReportCard from "./admin-card-components/UserReportCard";
 import { tutorValidationCardData, reportCardData } from "../../../dummyData";
 import { CloseOutlined } from "@mui/icons-material";
+import { Outlet } from "react-router-dom";
 
 export default function AdminCards() {
-  const [displayState, setDisplayState] = useState(false);
-  const [dataValidation, setDataValidation] = useState({
-    key: -1,
-    fullname: "",
-    username: "",
-    profession: "",
-    imgAvatarURL: "",
-    imgBgURL: "",
-  });
-
   return (
-    <>
-      <div className="myCardList">
-        {tutorValidationCardData.map((data, key) => {
-          return (
-            <TutorValidationCard
-              key={key}
-              fullname={data.fullname}
-              username={data.username}
-              profession={data.profession}
-              imgAvatarURL={data.imgAvatarURL}
-              imgBgURL={data.imgBgURL}
-              setTriggerView={setDisplayState}
-              setTriggerData={setDataValidation}
-            />
-          );
-        })}
-        {reportCardData.map((data, key) => {
-          return (
-            <UserReportCard
-              key={key}
-              header={data.header}
-              username={data.username}
-              detail={data.detail}
-              imgTopicURL={data.imgTopicURL}
-              imgBgURL={data.imgBgURL}
-            />
-          );
-        })}
-        <ViewPopup
-          data={dataValidation}
-          trigger={displayState}
-          setTrigger={setDisplayState}
-        />
-      </div>
-    </>
+    <div className="myCardList">
+      <Outlet />
+    </div>
   );
 }
 
@@ -74,5 +33,105 @@ function ViewPopup(props) {
     </div>
   ) : (
     ""
+  );
+}
+
+export function AllCards() {
+  const [displayState, setDisplayState] = useState(false);
+  const [dataValidation, setDataValidation] = useState({
+    key: -1,
+    fullname: "",
+    username: "",
+    profession: "",
+    imgAvatarURL: "",
+    imgBgURL: "",
+  });
+  return (
+    <>
+      {tutorValidationCardData.map((data, key) => {
+        return (
+          <TutorValidationCard
+            key={key}
+            fullname={data.fullname}
+            username={data.username}
+            profession={data.profession}
+            imgAvatarURL={data.imgAvatarURL}
+            imgBgURL={data.imgBgURL}
+            setTriggerView={setDisplayState}
+            setTriggerData={setDataValidation}
+          />
+        );
+      })}
+      {reportCardData.map((data, key) => {
+        return (
+          <UserReportCard
+            key={key}
+            header={data.header}
+            username={data.username}
+            detail={data.detail}
+            imgTopicURL={data.imgTopicURL}
+            imgBgURL={data.imgBgURL}
+          />
+        );
+      })}
+      <ViewPopup
+        data={dataValidation}
+        trigger={displayState}
+        setTrigger={setDisplayState}
+      />
+    </>
+  );
+}
+export function TutorValidationCards() {
+  const [displayState, setDisplayState] = useState(false);
+  const [dataValidation, setDataValidation] = useState({
+    key: -1,
+    fullname: "",
+    username: "",
+    profession: "",
+    imgAvatarURL: "",
+    imgBgURL: "",
+  });
+  return (
+    <>
+      {tutorValidationCardData.map((data, key) => {
+        return (
+          <TutorValidationCard
+            key={key}
+            fullname={data.fullname}
+            username={data.username}
+            profession={data.profession}
+            imgAvatarURL={data.imgAvatarURL}
+            imgBgURL={data.imgBgURL}
+            setTriggerView={setDisplayState}
+            setTriggerData={setDataValidation}
+          />
+        );
+      })}
+      <ViewPopup
+        data={dataValidation}
+        trigger={displayState}
+        setTrigger={setDisplayState}
+      />
+    </>
+  );
+}
+
+export function UserReportCards() {
+  return (
+    <>
+      {reportCardData.map((data, key) => {
+        return (
+          <UserReportCard
+            key={key}
+            header={data.header}
+            username={data.username}
+            detail={data.detail}
+            imgTopicURL={data.imgTopicURL}
+            imgBgURL={data.imgBgURL}
+          />
+        );
+      })}
+    </>
   );
 }
