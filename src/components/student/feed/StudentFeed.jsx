56a251ea5dac27266821a46a5c90c589a3f62e46
shrria <58872002+shrria@users.noreply.github.com>
2022-02-12 03:49:08 +0700

    Copy from Tutor's

--- src/components/student/feed/StudentFeed.jsx
+++ src/components/student/feed/StudentFeed.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import "./studentFeed.css";
-// import StudentCards from "./StudentCards";
+import { AutoAwesomeMotionOutlined, TaskOutlined } from "@mui/icons-material";
+import { MyCourses } from "./student-feed-widgets/MyCourse";
+import { Appts } from "./student-feed-widgets/ApptReq";
+import { myCourseData, apptData } from "../../../dummyData";
+import { Outlet } from "react-router-dom";
 
 export default function StudentFeed() {
   return (
     <div className="feed">
-      {/* <StudentCards /> */}
+      <div className="feedWrapper">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export function MyCrsCompSt() {
+  return (
+    <div className="feedMenu">
+      <div className="feedTitle">
+        <div className="feedTitleLeft">
+          <div className="feedTitleName">My Courses</div>
+          <div className="feedTitleBadge">{myCourseData.length}</div>
+        </div>
+        <div className="feedTitleRight">
+          <AutoAwesomeMotionOutlined className="feedTitleIcon" />
+        </div>
+      </div>
+      <div className="feedWidgetWrapper">
+        <MyCourses />
+      </div>
+    </div>
+  );
+}
+
+export function EnrollReqCompSt() {
+  return (
+    <div className="feedMenu">
+      <div className="feedTitle">
+        <div className="feedTitleLeft">
+          <div className="feedTitleName">Enrollment Requests</div>
+          <div className="feedTitleBadge">{apptData.length}</div>
+        </div>
+        <div className="feedTitleRight">
+          <TaskOutlined className="feedTitleIcon" />
+        </div>
+      </div>
+      <div className="feedWidgetWrapper">
+        <Appts />
+      </div>
     </div>
   );
 }
--- src/components/student/sidebar/StudentSidebar.jsx
+++ src/components/student/sidebar/StudentSidebar.jsx
@@ -7,6 +7,7 @@ import {
   HelpOutline,
   Event,
 } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 export default function StudentSidebar() {
   return (
@@ -15,29 +16,45 @@ export default function StudentSidebar() {
         <ul className="studentSidebarList">
           <li className="studentSidebarListItem">
             <HistoryEduOutlined className="studentSidebarIcon" />
-            <span className="studentSidebarListItemText">My Courses</span>
+
+            <Link className="studentSidebarListItemText" to="/student/mycourse">
+              My Courses
+            </Link>
           </li>
 
           <li className="studentSidebarListItem">
             <GroupAddRounded className="studentSidebarIcon" />
-            <span className="studentSidebarListItemText">
+
+            <Link
+              className="studentSidebarListItemText"
+              to="/student/enrollreq"
+            >
               Enrollment Requests
-            </span>
+            </Link>
           </li>
 
           <li className="studentSidebarListItem">
             <PaymentsRounded className="studentSidebarIcon" />
-            <span className="studentSidebarListItemText">Payment</span>
+
+            <Link className="studentSidebarListItemText" to="/student">
+              Payment
+            </Link>
           </li>
 
           <li className="studentSidebarListItem">
             <HelpOutline className="studentSidebarIcon" />
-            <span className="studentSidebarListItemText">FAQ</span>
+
+            <Link className="studentSidebarListItemText" to="/student">
+              FAQ
+            </Link>
           </li>
 
           <li className="studentSidebarListItem">
             <Event className="studentSidebarIcon" />
-            <span className="studentSidebarListItemText">Events</span>
+
+            <Link className="studentSidebarListItemText" to="/student">
+              Events
+            </Link>
           </li>
         </ul>
         <hr className="studentSidebarHr" />
