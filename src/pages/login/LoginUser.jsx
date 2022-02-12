8b734c89299d52f597e847f9a48a4fc812c76309
shrria <58872002+shrria@users.noreply.github.com>
2022-02-13 01:41:36 +0700

    Update LoginUser.jsx

--- src/pages/login/LoginUser.jsx
+++ src/pages/login/LoginUser.jsx
@@ -3,21 +3,26 @@ import LoginRightside from "../../components/login/loginCardRight";
 import Navbar from "../../components/simple/navbar/Navbar";
 import Footer from "../../components/simple/footer/Footer";
 import "./loginUser.css";
-import { useState, useEffect, } from "react";
+import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 export default function LoginUser({ cookie, setCookie }) {
   const [state, setState] = useState(false);
   useEffect(() => {
     // Update the document title using the browser API
-    if (cookie.user !== undefined && cookie.user !== null && cookie.user !=='') {
+    if (
+      cookie.user !== undefined &&
+      cookie.user !== null &&
+      cookie.user !== ""
+    ) {
       setState(true);
     }
   }, [state]);
+
   return (
     <>
-    {state && cookie.user_role ===  'admin' && <Navigate to='/admin'/>}
-    {state && cookie.user_role ===  'tutor' && <Navigate to='/tutor'/>}
-    {state && cookie.user_role ===  'student' && <Navigate to='/student'/>}
+      {state && cookie.user_role === "admin" && <Navigate to="/admin" />}
+      {state && cookie.user_role === "tutor" && <Navigate to="/tutor" />}
+      {state && cookie.user_role === "student" && <Navigate to="/student" />}
 
       <div className="login">
         <Navbar />
