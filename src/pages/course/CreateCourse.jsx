47f80ed7946a6817df98601ba141da96f5443db6
shrria <58872002+shrria@users.noreply.github.com>
2022-02-14 04:11:10 +0700

    Create createCourse.css
    remove tutorHome.css in course

--- src/pages/course/CreateCourse.jsx
+++ src/pages/course/CreateCourse.jsx
@@ -1,7 +1,6 @@
-import "./tutorHome.css";
+import "./createCourse.css";
 import TutorTopbar from "../../components/tutor/topbar/TutorTopbar";
 import TutorSidebar from "../../components/tutor/sidebar/TutorSidebar";
-import TutorFeed from "../../components/tutor/feed/TutorFeed";
 import MainCard from "../../components/course/mainCard";
 
 export default function CreateCourse() {
