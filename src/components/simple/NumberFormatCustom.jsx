bbc99256b52a2438d47e9dba375906d838abc5c3
shrria <58872002+shrria@users.noreply.github.com>
2022-02-14 11:37:55 +0700

    Create NumberFormatCustom.jsx

---
+++ src/components/simple/NumberFormatCustom.jsx
@@ -0,0 +1,32 @@
+import React, { forwardRef } from "react";
+import PropTypes from "prop-types";
+import NumberFormat from "react-number-format";
+
+export const NumberFormatCustom = forwardRef(function NumberFormatCustom(
+  props,
+  ref
+) {
+  const { onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={ref}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
+      }}
+      thousandSeparator
+      isNumericString
+    />
+  );
+});
+
+NumberFormatCustom.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
