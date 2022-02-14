2aa229f9b14bdc9653e053af3747b0068f8ee628
shrria <58872002+shrria@users.noreply.github.com>
2022-02-14 11:13:31 +0700

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminHome from "./pages/admin/home/AdminHome";
-import TutorHome from "./pages/tutor/home/TutorHome";
-import StudentHome from "./pages/student/home/StudentHome";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import CreateCourse from "./pages/course/CreateCourse";
 
 import App from "./App";
 
