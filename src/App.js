1d18f11513dd7bf55c8d2dbe039de18e1c2d11a6
shrria <58872002+shrria@users.noreply.github.com>
2022-02-14 07:07:57 +0700

    Update App.js
    import Redirect

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import StudentHome from "./pages/student/home/StudentHome";
 import LoginUser from "./pages/login/LoginUser";
 import LoginAdmin from "./pages/login/LoginAdmin";
 import Register from "./pages/register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import {
   MyCrsComp,
   EnrollReqComp,
