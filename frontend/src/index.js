69d923f17c9998756f0296e6f8145ce254447c01
shrria <58872002+shrria@users.noreply.github.com>
2022-02-15 04:06:19 +0700

    Delete unuseds

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
