b25d37ec1510c9aad0ab5c539f8a766c95902fdd
shrria <58872002+shrria@users.noreply.github.com>
2022-02-15 03:03:48 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ import StudentHome from "./pages/student/home/StudentHome";
 import LoginUser from "./pages/login/LoginUser";
 import LoginAdmin from "./pages/login/LoginAdmin";
 import Register from "./pages/register/Register";
-import CreateCourse from "./pages/course/CreateCourse";
 
 import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import {
@@ -228,17 +227,6 @@ function App() {
           path="register"
           element={
             <Register
-              cookie={cookie}
-              setCookie={setCookie}
-              removeCookie={removeCookie}
-            />
-          }
-        />
-
-        <Route
-          path="course/create"
-          element={
-            <CreateCourse
               cookie={cookie}
               setCookie={setCookie}
               removeCookie={removeCookie}
