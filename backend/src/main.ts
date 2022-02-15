dca0e72bd19c6be629ff8998a32f289796d375c3
shrria <58872002+shrria@users.noreply.github.com>
2022-02-15 23:52:23 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { urlencoded, json } from 'express';
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 
 process.on('unhandledRejection', (reason, p) => {
   // console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
@@ -11,15 +11,16 @@ process.on('unhandledRejection', (reason, p) => {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['welovese2']
-    })
+  app.use(
+    cookieSession({
+      keys: ['welovese2'],
+    }),
   );
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true
-    })
+      whitelist: true,
+    }),
   );
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
