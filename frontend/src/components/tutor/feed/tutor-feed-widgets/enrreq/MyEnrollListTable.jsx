2548079f3627538e2b855cf6533bd4998e9a52a6
shrria <58872002+shrria@users.noreply.github.com>
2022-02-16 21:20:34 +0700

    Fetch EnrollList from backend
    only first row

--- frontend/src/components/tutor/feed/tutor-feed-widgets/enrreq/MyEnrollListTable.jsx
+++ frontend/src/components/tutor/feed/tutor-feed-widgets/enrreq/MyEnrollListTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { CloseOutlined } from "@mui/icons-material";
+import { CloseOutlined, SettingsPhoneRounded } from "@mui/icons-material";
 import UpdateCourseCard from "./update/UpdateCourseCard";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
@@ -8,58 +8,49 @@ import "./myEnrollListTable.css";
 export default function MyEnrollListTable({ cookie, setCookie, removeCookie }) {
   const [push, setPush] = useState(false);
 
+  const [course, setCourse] = useState({});
+
+  var [enroll, setEnroll] = useState([]);
+
   useEffect(() => {
-    axios
-      .get(`http://localhost:3000/enroll/waiting/${cookie.user}`, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        // const data = response.data
-        setCourse(response.data);
-        // console.log(response);
-      })
-      .catch((e) => {
+    const FetchAPI = async () => {
+      try {
+        axios
+          .get(`http://localhost:3000/enroll/waiting/tutor/${cookie.user}`, {
+            withCredentials: true,
+          })
+          .then((response) => {
+            setCourse(response.data);
+
+            const getRowAddInfo = async (eachRow, i) => {
+              axios
+                .get(`http://localhost:3000/course/${eachRow.courseId}`, {
+                  withCredentials: true,
+                })
+                .then((response2) => {
+                  return Object.assign({}, eachRow, {
+                    studentName:
+                      eachRow.studentFirstName + " " + eachRow.studentLastName,
+                    ...response2.data,
+                    studentCount: response2.data.students.length,
+                  });
+                })
+                .then((tmp) => {
+                  setEnroll(Object.assign([], enroll, { [i]: tmp }));
+                });
+            };
+
+            for (let i = 0; i < 1; i++) {
+              getRowAddInfo(response.data[i], i.toString());
+            }
+          });
+      } catch (e) {
         console.log(e);
-      });
+      }
+    };
+
+    FetchAPI();
   }, [push]);
-
-  const [course, setCourse] = useState(null);
-
-  const [displayState, setDisplayState] = useState(false);
-
-  const [courseId, setCourseID] = useState(null);
-
-  const [enroll, setEnroll] = useState({
-    id: "",
-    courseName: "",
-    subject: "",
-    lesson: "",
-    price: "",
-    timeSlots: "",
-    capacity: "",
-    status: "",
-    learningType: "",
-  });
-
-  // const handleChange = (prop) => (event) => {
-  //   setEnroll({
-  //     ...enroll,
-  //     [event.target.name]: event.target.value,
-  //   });
-  // };
-
-  const handleEdit = (id) => {
-    // console.log()
-    axios
-      .get(`http://localhost:3000/course/${id}`, { withCredentials: true })
-      .then((response) => {
-        setEnroll(response.data);
-        setCourseID(id);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
 
   const handleApprove = (isApproved, id) => {
     console.log(id);
@@ -81,19 +72,19 @@ export default function MyEnrollListTable({ cookie, setCookie, removeCookie }) {
   };
 
   const columns = [
-    // { field: "_id", headerName: "ID", width: 80 },
-    { field: "studentName", headerName: "Student Name", width: 280 },
+    { field: "studentName", headerName: "Student Name", width: 200 },
     { field: "courseName", headerName: "Course Name", width: 200 },
-    { field: "lesson", headerName: "Lesson", width: 220 },
+    { field: "subject", headerName: "Subject", width: 150 },
+    { field: "lesson", headerName: "Lesson", width: 150 },
     {
-      field: "price",
-      headerName: "Price ฿",
-      width: 150,
+      field: "studentCount",
+      headerName: "Student Count",
+      width: 140,
     },
     {
       field: "capacity",
       headerName: "Capacity",
-      width: 100,
+      width: 140,
     },
     {
       field: "action",
@@ -103,32 +94,22 @@ export default function MyEnrollListTable({ cookie, setCookie, removeCookie }) {
         return (
           <>
             <button
-              className="courseEditButton"
+              className="enrollApproveButton"
               onClick={() => {
-                setDisplayState(true);
-                setEnroll({ id: params.id });
-                handleEdit(params.id);
-                // console.log(params.id);
+                handleApprove(true, params.id);
               }}
             >
-              Edit
+              Approved
             </button>
-            {params.row.status === "rejected" && (
-              <button
-                className="coursePublishButton"
-                onClick={() => handleApprove(false, params.id)}
-              >
-                Rejected
-              </button>
-            )}
-            {params.row.status === "approved" && (
-              <button
-                className="coursePublishButton"
-                onClick={() => handleApprove(true, params.id)}
-              >
-                Approved
-              </button>
-            )}
+
+            <button
+              className="enrollRejectButton"
+              onClick={() => {
+                handleApprove(false, params.id);
+              }}
+            >
+              Rejected
+            </button>
           </>
         );
       },
@@ -136,51 +117,17 @@ export default function MyEnrollListTable({ cookie, setCookie, removeCookie }) {
   ];
 
   return (
-    <div className="mainCourse">
-      <div className="mainCourseTitle">Enrollment Request</div>
-      <div className="mainCourseTable">
+    <div className="mainEnroll">
+      <div className="mainEnrollTitle">Enrollment Request</div>
+      <div className="mainEnrollTable">
         <DataGrid
-          rows={course}
+          rows={enroll}
           columns={columns}
-          getRowId={(r) => r._id}
+          getRowId={(row) => row.id}
           disableSelectionOnClick
           checkboxSelection
-          setTrigger={setDisplayState}
-        />
-      </div>
-      <EditCoursePopup
-        className=""
-        trigger={displayState}
-        setTrigger={setDisplayState}
-        data={enroll}
-        setData={setEnroll}
-        id={courseId}
-      />
-    </div>
-  );
-}
-
-function EditCoursePopup(props) {
-  // console.log(props)
-  return props.trigger ? (
-    <div className="viewEditCoursePopup">
-      <div className="viewEditCoursePopupContainer">
-        <button
-          className="closeEditCoursePopup"
-          onClick={() => props.setTrigger(false)}
-        >
-          <CloseOutlined />
-        </button>
-        <UpdateCourseCard
-          id={props.id}
-          data={props.data}
-          firstname={props.firstname}
-          setTrigger={props.setTrigger}
-          setData={props.setData}
         />
       </div>
     </div>
-  ) : (
-    ""
   );
 }
