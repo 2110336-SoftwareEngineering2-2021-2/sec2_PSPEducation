271e5f27fadccd172450e93e8d3a43c78091e7a7
shrria <58872002+shrria@users.noreply.github.com>
2022-02-16 14:43:21 +0700

    Update Register.jsx

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -6,19 +6,26 @@ import RegisterCard from "../../components/register/RegisterCard.jsx";
 import "./register.css";
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-export default function Register({ cookie, setCookie }) {
+
+export default function Register({ cookie, setCookie, removeCookie }) {
   const [state, setState] = useState(false);
+
   useEffect(() => {
     // Update the document title using the browser API
-    if (cookie.user !== undefined && cookie.user !== null && cookie.user !=='') {
+    if (
+      cookie.user !== undefined &&
+      cookie.user !== null &&
+      cookie.user !== ""
+    ) {
       setState(true);
     }
   }, [state]);
+
   return (
     <>
-    {state && cookie.user_role ===  'admin' && <Navigate to='/admin'/>}
-    {state && cookie.user_role ===  'tutor' && <Navigate to='/tutor'/>}
-    {state && cookie.user_role ===  'student' && <Navigate to='/student'/>}
+      {state && cookie.user_role === "admin" && <Navigate to="/admin" />}
+      {state && cookie.user_role === "tutor" && <Navigate to="/tutor" />}
+      {state && cookie.user_role === "student" && <Navigate to="/student" />}
 
       <div className="register">
         <Navbar />
