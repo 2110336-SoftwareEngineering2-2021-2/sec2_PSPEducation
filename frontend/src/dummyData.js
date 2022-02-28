01ceccc5f7c442c98118a8ecca9a3c2379249d69
shrria <58872002+shrria@users.noreply.github.com>
2022-03-01 03:30:19 +0700

    Update dummyData.js

--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,13 +1,11 @@
-export const apptData = [
-  {
+export const apptData = [{
     fullname: "Yotsuba Nakano",
     username: "@yotsuba428",
     day: "Monday",
     date: "17 Jan 2022",
     time: "16:30-18:30",
     location: "at Living Room",
-    imgURL:
-      "https://somoskudasai.com/wp-content/uploads/2020/12/yotsuba-nakano-somoskudasai.jpg",
+    imgURL: "https://somoskudasai.com/wp-content/uploads/2020/12/yotsuba-nakano-somoskudasai.jpg",
   },
   {
     fullname: "Miku Nakano",
@@ -16,13 +14,11 @@ export const apptData = [
     date: "18 Jan 2022",
     time: "10:00-12:00",
     location: "at School Library",
-    imgURL:
-      "https://i.pinimg.com/736x/1c/4f/4c/1c4f4c7010a0b7ca14e32baa83fe2181.jpg",
+    imgURL: "https://i.pinimg.com/736x/1c/4f/4c/1c4f4c7010a0b7ca14e32baa83fe2181.jpg",
   },
 ];
 
-export const myCourseData = [
-  {
+export const myCourseData = [{
     course_name: "Badminton w/ AongAong",
     status: "Newly Open",
     registered_num: 1,
@@ -42,26 +38,21 @@ export const myCourseData = [
   },
 ];
 
-export const msgData = [
-  {
-    fullname: "Itsuki Nakano",
-    username: "@it-tsuki5",
-    content: "Uesugu-kun, we'll be waiting at Cafe.",
-    imgURL:
-      "http://pm1.narvii.com/7164/138f64a909260c53492ff549125858e140372130r1-750-750v2_00.jpg",
-  },
-];
+export const msgData = [{
+  fullname: "Itsuki Nakano",
+  username: "@it-tsuki5",
+  content: "Uesugu-kun, we'll be waiting at Cafe.",
+  imgURL: "http://pm1.narvii.com/7164/138f64a909260c53492ff549125858e140372130r1-750-750v2_00.jpg",
+}, ];
 
 export const userData = {
   fullname: "Fuutarou Uesugi",
   username: "@futaro_15",
   credit_balance: 100.0,
-  imgURL:
-    "https://i.pinimg.com/564x/2c/82/0d/2c820d2ea88ad5c92141e282074ddfe7.jpg",
+  imgURL: "https://i.pinimg.com/564x/2c/82/0d/2c820d2ea88ad5c92141e282074ddfe7.jpg",
 };
 
-export const tutorValidationCardData = [
-  {
+export const tutorValidationCardData = [{
     firstname: "Steve",
     lastname: "Rogers",
     username: "@steve.rogers",
@@ -72,11 +63,9 @@ export const tutorValidationCardData = [
     gender: "male",
     educationLevel: "6",
     citizenId: "123XXXXXXXXXX",
-    citizenImage:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/c/c3/S.H.I.E.L.D._Rogers_file.jpg",
+    citizenImage: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/c/c3/S.H.I.E.L.D._Rogers_file.jpg",
     profession: "Shielded Hero",
-    imgAvatarURL:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/ac/154545385l.jpg",
+    imgAvatarURL: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/ac/154545385l.jpg",
     imgAvatarBase64: "https://source.unsplash.com/random/300×300/?face",
     imgBgURL: "https://source.unsplash.com/random/800×300/?america",
   },
@@ -91,11 +80,9 @@ export const tutorValidationCardData = [
     gender: "female",
     educationLevel: "6",
     citizenId: "123XXXXXXXXXX",
-    citizenImage:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/1/16/Skye_File.jpg",
+    citizenImage: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/1/16/Skye_File.jpg",
     profession: "EarthQuake",
-    imgAvatarURL:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/7/79/Quake.jpg",
+    imgAvatarURL: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/7/79/Quake.jpg",
     imgAvatarBase64: "https://source.unsplash.com/random/300×300/?face",
     imgBgURL: "https://source.unsplash.com/random/800×300/?ruins",
   },
@@ -110,11 +97,9 @@ export const tutorValidationCardData = [
     gender: "male",
     educationLevel: "7",
     citizenId: "123XXXXXXXXXX",
-    citizenImage:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/d/d6/Clint_Barton_File.jpg",
+    citizenImage: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/d/d6/Clint_Barton_File.jpg",
     profession: "Expert Archery",
-    imgAvatarURL:
-      "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/8/86/018hcb_ons_inl_04.jpg",
+    imgAvatarURL: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/8/86/018hcb_ons_inl_04.jpg",
     imgAvatarBase64: "https://source.unsplash.com/random/300×300/?face",
     imgBgURL: "https://source.unsplash.com/random/800×300/?archer",
   },
@@ -132,77 +117,64 @@ export const educationTable = {
   8: "Doctor Degree",
 };
 
-export const reportCardData = [
-  {
-    header: "Login Problems",
+export const reportCardData = [{
+    title: "Login Problems",
     username: "@aong.aong",
-    detail:
-      "I forgot my password, and there's no reset password function on Login page",
+    detail: "I forgot my password, and there's no reset password function on Login page",
     imgTopicURL: "https://source.unsplash.com/random/300×300/?login",
     imgBgURL: "https://source.unsplash.com/random/800×300/?workspace",
   },
   {
-    header: "Cannot withdraw credit",
+    title: "Cannot withdraw credit",
     username: "@psp.master",
-    detail:
-      "I cannot withdraw 9999 credit from my tutor account to bank account",
+    detail: "I cannot withdraw 9999 credit from my tutor account to bank account",
     imgTopicURL: "https://source.unsplash.com/random/300×300/?money",
     imgBgURL: "https://source.unsplash.com/random/800×300/?currency",
   },
 ];
 
-export const developerCardData = [
-  {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/218005260080381955/ffb4791856ab7bd06d1c6cf3407911d1.webp?size=32",
+export const developerCardData = [{
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/218005260080381955/ffb4791856ab7bd06d1c6cf3407911d1.webp?size=32",
     devName: "Molk",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/552812105745825802/2f64f03ece9d9b911b20e45e27d0fd2c.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/552812105745825802/2f64f03ece9d9b911b20e45e27d0fd2c.webp?size=32",
     devName: "Mames",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/195929709882638336/ee81982d99e5b0e4c6268ba38b8b7ac4.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/195929709882638336/ee81982d99e5b0e4c6268ba38b8b7ac4.webp?size=32",
     devName: "Mong",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/274922997377138689/e8108d297ef173188f3cf26a6f0fc000.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/274922997377138689/e8108d297ef173188f3cf26a6f0fc000.webp?size=32",
     devName: "MeeMut",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/372376899923476510/bfec971642c33c2346dd1479d68404aa.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/372376899923476510/bfec971642c33c2346dd1479d68404aa.webp?size=32",
     devName: "Mact",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/217651898533019648/d090f361f81f56bfd95f796d0996a5e7.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/217651898533019648/d090f361f81f56bfd95f796d0996a5e7.webp?size=32",
     devName: "Moey",
   },
   {
-    imgAvatarURL:
-      "https://i.pinimg.com/originals/7c/8f/47/7c8f476123d28d103efe381543274c25.png",
+    imgAvatarURL: "https://i.pinimg.com/originals/7c/8f/47/7c8f476123d28d103efe381543274c25.png",
     devName: "Mron",
   },
   {
-    imgAvatarURL:
-      "https://cdn.discordapp.com/avatars/367612200157970432/d718cabcc2e0147863f6b23581e012cc.webp?size=32",
+    imgAvatarURL: "https://cdn.discordapp.com/avatars/367612200157970432/d718cabcc2e0147863f6b23581e012cc.webp?size=32",
     devName: "Mhane",
   },
 ];
 
-export const listCourseRows = [
-  {
+export const listCourseRows = [{
     id: 1,
     courseName: "Intania",
     subject: "Com",
     lesson: "Pypi",
     price: 2600,
     capacity: 26,
-    fdasdf: 1
+    fdasdf: 1,
   },
   {
     id: 2,
